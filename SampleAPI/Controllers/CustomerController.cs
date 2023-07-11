94634956ca7c8a7b540eb64f9a8eb7087e75b336
AjaySingala <ajay.singala@gmail.com>
2023-07-11 15:41:58 +0530

    Customer API

---
+++ SampleAPI/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Entities;
+using SampleAPI.Repositories;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        ICustomerRepository _repo;
+
+        public CustomerController()
+        {
+            _repo = new CustomerRepository();
+        }
+
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return _repo.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Customer Get(int id)
+        {
+            return _repo.Get(id);
+        }
+    }
+}
